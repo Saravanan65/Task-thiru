64ff4a4ccf280a37498718389ce13f67570b6642
Saravanan65 <136047134+Saravanan65@users.noreply.github.com>
2023-07-25 12:10:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var res = fetch("https://restcountries.com/v2/all");
+res.then((data) => data.json()).then((data1) => {
+    for (var i = 0; i < data1.length; i++) {
+        console.log(data1[i]);
+        var div = document.createElement("div");
+        div.innerHTML = `  <div class="row row-cols-1 row-cols-md-4 g-3">
+        <div class="col">
+          <div class="card">
+            <img src="${data1[i].flag}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${data1[i].name}</h5>
+              <p class="card-text">${data1[i].region}</p>
+              <pclass="card-text">${data1[i].subregion}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+        `
+
+    document.body.append(div);
+
+    }
+
+});
